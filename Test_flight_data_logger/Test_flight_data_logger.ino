0f4bf77f9a048805586154fda33d3d299e3bfc97
John Hoeksema <johnhoeksema@johns-mbp.dhcp.nd.edu>
2018-11-11 17:05:20 -0500

    Subscale code

---
+++ Test_flight_data_logger/Test_flight_data_logger.ino
@@ -0,0 +1,16 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "SparkFunMPL3115A2.h"
+#include <Adafruit_L3GD20_U.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
